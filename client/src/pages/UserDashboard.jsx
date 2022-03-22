964fd991e3a47e611b9d57ef630f7c9ec51da8ad
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-03-22 09:35:10 +0630

    new

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DataTable from "react-data-table-component";
+import Layout from "../Layout";
 
 const columns = [
   {
@@ -49,28 +50,7 @@ const UserDashboard = () => {
   };
 
   return (
-    <div className="w-full h-screen">
-      <div className="w-full h-16 bg-gray-300">Logo</div>
-      <button
-        className="absolute right-5 top-3 flex items-center justify-center w-10 h-10 rounded-full bg-gray-200"
-        onClick={logoutHandler}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-          />
-        </svg>
-      </button>
-
+    <Layout>
       <div className="w-full flex">
         <div className="w-1/2">
           <div className="px-4 pt-4">
@@ -97,7 +77,7 @@ const UserDashboard = () => {
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
